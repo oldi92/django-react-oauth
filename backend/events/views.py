2bfa6ffd915943e7e57ff35bf866578be7d5ebe6
Oldi Prillo <oldi.prillo@gmail.com>
2023-05-13 13:23:04 +0100

    Successful build calendar service from authentication code

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,4 +1,4 @@
-from allauth.socialaccount.models import SocialToken, SocialApp, SocialAccount, SocialLogin
+from allauth.socialaccount.models import SocialToken, SocialApp
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from rest_framework.response import Response
@@ -6,16 +6,9 @@ from rest_framework.views import APIView
 import datetime
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from accounts.models import CustomUser
 from allauth.socialaccount.providers.base.mixins import OAuthLoginMixin
 from allauth.socialaccount.providers.oauth2.views import OAuth2View
-from django.http import HttpResponseRedirect
-from allauth.socialaccount.providers.google.views import oauth2_login, oauth2_callback
-
-from dj_rest_auth.registration.views import SocialLoginView, SocialConnectView
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client, OAuth2Error
-import jwt
+import requests
 
 
 class Events(APIView):
@@ -67,101 +60,51 @@ class Events(APIView):
                                               orderBy='startTime').execute()
         return Response(events_result)
 
-# class OAuth2LoginView(OAuthLoginMixin, OAuth2View):
-#     def login(self, request, *args, **kwargs):
-        # provider = self.adapter.get_provider()
-        # app = provider.get_app(self.request)
-        # client = self.get_client(request, app)
-        # action = request.GET.get("action", AuthAction.AUTHENTICATE)
-        # auth_url = self.adapter.authorize_url
-        # auth_params = provider.get_auth_params(request, action)
 
-        # pkce_params = provider.get_pkce_params()
-        # code_verifier = pkce_params.pop("code_verifier", None)
-        # auth_params.update(pkce_params)
-        # if code_verifier:
-        #     request.session["pkce_code_verifier"] = code_verifier
+class SocialCalendar(APIView, OAuthLoginMixin, OAuth2View):
+    def get_google_social_app(self):
+        social_app = SocialApp.objects.get(provider='google')
+        return social_app
 
-        # client.state = SocialLogin.stash_state(request)
-        # try:
-        #     return HttpResponseRedirect(client.get_redirect_url(auth_url, auth_params))
-        # except OAuth2Error as e:
-        #     return render_authentication_error(request, provider.id, exception=e)
+    def build_service(self, token, refresh_token):
+        social_app = self.get_google_social_app()
 
+        credentials = Credentials(
+            token,
+            refresh_token=refresh_token,
+            token_uri='https://oauth2.googleapis.com/token',
+            client_id=social_app.client_id,
+            client_secret=social_app.secret,
+            scopes=[
+                'https://www.googleapis.com/auth/calendar'
+            ]
+        )
 
-class CustomGoogleOAuth2Adapter(GoogleOAuth2Adapter):
-    '''
-    Custom google Oauth2 adapter because there is a open issue on dj-rest-auth
-    which trying to grab id_token from response but response is dictionary
+        service = build('calendar', 'v3', credentials=credentials)
+        return service
 
-    More info
-    https://github.com/iMerica/dj-rest-auth/issues/490
-    '''
+    def post(self, request):
+        code = request.data['code']
+        token_url = "https://oauth2.googleapis.com/token"
 
-    def complete_login(self, request, app, token, response, **kwargs):
-        try:
+        social_app = SocialApp.objects.get(provider='google')
+        client_id = social_app.client_id
+        client_secret = social_app.secret
+        data = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": "http://localhost:3000/dashboard",
+            "client_id": client_id,
+            "client_secret": client_secret
+        }
+        response = requests.post(token_url, data)
+        data = response.json()
+        access_token = data['access_token']
+        refresh_token = data['refresh_token']
 
-            identity_data = jwt.decode(
-                response["id_token"]['id_token'],
-                # Since the token was received by direct communication
-                # protected by TLS between this library and Google, we
-                # are allowed to skip checking the token signature
-                # according to the OpenID Connect Core 1.0
-                # specification.
-                # https://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation
-                options={
-                    "verify_signature": False,
-                    "verify_iss": True,
-                    "verify_aud": True,
-                    "verify_exp": True,
-                },
-                issuer=self.id_token_issuer,
-                audience=app.client_id,
-            )
-
-            print('********** ')
-            print('CREATE CALENDAR ', identity_data)
-            print('********** ')
-        except jwt.PyJWTError as e:
-            raise OAuth2Error("Invalid id_token") from e
-        login = self.get_provider().sociallogin_from_response(request, identity_data)
-        return login
-
-
-class SocialCalendar(SocialLoginView):
-    adapter_class = CustomGoogleOAuth2Adapter
-    client_class = OAuth2Client
-    callback_url = 'http://localhost:3000'
-
-
-# class SocialCalendar(APIView, OAuthLoginMixin, OAuth2View):
-#     def post(self, request):
-#         login = oauth2_login(request)
-#         callback = oauth2_callback(request)
-#         # code = request.data['code']
-#         # provider = self.adapter.get_provider()
-#         # app = provider.get_app(self.request)
-#         # client = self.get_client(request, app)
-#         # action = request.GET.get("action", AuthAction.AUTHENTICATE)
-#         # auth_url = self.adapter.authorize_url
-#         # auth_params = provider.get_auth_params(request, action)
-
-#         # pkce_params = provider.get_pkce_params()
-#         # code_verifier = pkce_params.pop("code_verifier", None)
-#         # auth_params.update(pkce_params)
-#         # if code_verifier:
-#         #     request.session["pkce_code_verifier"] = code_verifier
-
-#         # client.state = SocialLogin.stash_state(request)
-#         # response = HttpResponseRedirect(
-#         #     client.get_redirect_url(auth_url, auth_params))
-#         # except OAuth2Error as e:
-#         #     return render_authentication_error(request, provider.id, exception=e)
-
-#         # SocialLogin.stash_state(request)
-#         print('***************')
-#         print("RESPONSE ", login)
-#         print("CALLBACK ", callback)
-#         print('***************')
-
-#         return Response('Calendar created')
+        service = self.build_service(access_token, refresh_token)
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=5, singleEvents=True,
+                                              orderBy='startTime').execute()
+        return Response(events_result)
